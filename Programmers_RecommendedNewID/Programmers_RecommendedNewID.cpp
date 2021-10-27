6f4ab29ebb6c83eacb07584f469a9ff11d5f2330
Jiwon <jae2021@gmail.com>
2021-10-27 15:05:11 +0900

    20211027
    20211027

---
+++ Programmers_RecommendedNewID/Programmers_RecommendedNewID.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+string solution(string new_id);
+int main()
+{
+	cout << solution("...!@BaT#*..y.abcdefghijklm") << endl;
+}
+string solution(string new_id) {
+    string answer = new_id;
+    return answer;
+}
