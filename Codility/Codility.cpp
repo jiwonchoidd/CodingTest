486f8ec9cb78bb331ff5335da8a79bea300ea926
최지원 <popo2022@naver.com>
2022-07-04 19:29:04 +0900

    기업 코테 대비
    기업 코테 대비

---
+++ Codility/Codility.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <vector>
+/* 2022-07-28 최지원*/
+int solution(vector<int>& A) {
+    //버블 정렬
+    for (size_t i = 0; i < A.size() - 1; i++)
+    {
+        for (size_t j = 0; j < A.size() - i; j++)
+        {
+            if (A[j - 1] > A[j])
+            {
+                int temp = A[j - i];
+                A[j - i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+    for (size_t i = 1; i < A.size(); i++)
+    {
+        if (A[i - 1] > 0 && (A[i - 1] + 1) != A[i])
+        {
+            return (A[i - 1] + 1);
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
